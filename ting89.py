4b01d893ab911f8c8f4aa5531a525bde5b80ca10
宋志京 <>
2019-06-19 17:47:34 +0800

    添加search接口

--- ting89.py
+++ ting89.py
@@ -4,6 +4,30 @@ import json
 import urllib.parse
 
 class Ting89:
+    def search(self,name):
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {
+            'searchword':name,
+            'searchtype':'-1',
+            'submit':'搜索'
+        }
+        data_gb2312 = urllib.parse.urlencode(data, encoding='gb2312')
+        r = requests.post('http://www.ting89.com/search.asp', data=data_gb2312, headers=headers)
+        r.encoding = 'gb2312'
+        
+        sel = Selector(r.text)
+
+        albumList = []
+        for c in sel.xpath("//div[@class='clist']//li"):
+            album = {}
+            album['title']=c.xpath(".//b/text()").get()
+            album['author']=c.xpath(".//p[3]/text()").get()
+            album['sound']=c.xpath(".//p[4]/text()").get()
+            album['url']=urllib.parse.urljoin(r.url, c.xpath("./a/@href").get())
+            albumList.append(album)
+        
+        return albumList
+
     def getList(self,url):
         r=requests.get(url)
         r.encoding = 'gb2312' # 此网站不规范，只能手写
@@ -36,6 +60,7 @@ class Ting89:
 
 if __name__=='__main__':
     t = Ting89()
-    print(t.getUrl("http://www.ting89.com/books/13503.html",0))
+    # print(t.getUrl("http://www.ting89.com/books/13503.html",0))
     # print(t.getList("http://www.ting89.com/books/13503.html"))
+    print(t.search("阳间巡逻人"))
 
